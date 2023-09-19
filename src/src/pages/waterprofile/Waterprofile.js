11722455b109e27f48c5456c405e2f83dfe7294d
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-09-19 11:57:24 -0500

    EWP-102 content grouping implemented

--- src/src/pages/waterprofile/Waterprofile.js
+++ src/src/pages/waterprofile/Waterprofile.js
@@ -21,30 +21,35 @@ import {
   TileLayer,
   WMSTileLayer,
 } from "react-leaflet";
-import WaterpointItem from "../../components/waterpointItem/WaterpointItem";
 import html2canvas from "html2canvas";
 import JsPDF from "jspdf";
 import Services from "../../services/apiService";
 import { useTranslation } from "react-i18next";
+import ItemIconX from "../../components/itemIconX/ItemIconX";
+import ItemIconY from "../../components/itemIconY/ItemIconY";
+import ItemText from "../../components/itemText/ItemText";
+import ItemTable from "../../components/itemTable/ItemTable";
+import ItemSimpleList from "../../components/itemSimpleList/ItemSimpleList";
+import ItemComplexList from "../../components/itemComplexList/ItemComplexList";
 
 function Waterprofile() {
   const [t, i18n] = useTranslation("global");
   const location = useLocation();
   const idWater = location.state?.idWater;
+  const [wp, setWp] = useState();
   const [wpProfile, setWpProfile] = useState();
   const [loading, setLoading] = useState(true);
   const [wsTable, setWsTable] = useState(null);
-
-  const [wsLeft, setWsLeft] = useState(null);
-  const [wsRight, setWsRight] = useState(null);
-  const [wpLeft, setWpLeft] = useState(null);
-  const [wpRight, setWpRight] = useState(null);
 
   useEffect(() => {
     //Call to API to get waterpoint
     Services.get_waterpoints_profile(idWater, i18n.language)
       .then((response) => {
-        setWpProfile(response[0]);
+        setWp(response[0]);
+        setWpProfile({
+          contents_wp: response[0].contents_wp,
+          contents_ws: response[0].contents_ws,
+        });
         setLoading(false);
       })
       .catch((error) => {
@@ -53,23 +58,11 @@ function Waterprofile() {
   }, []);
 
   useEffect(() => {
-    if (wpProfile) {
-      setWsLeft(
-        wpProfile.contents_ws.filter((item) => item.position === "left")
-      );
-      setWsRight(
-        wpProfile.contents_ws.filter((item) => item.position === "right")
-      );
-      setWpLeft(
-        wpProfile.contents_wp.filter((item) => item.position === "left")
-      );
-      setWpRight(
-        wpProfile.contents_wp.filter((item) => item.position === "right")
-      );
-      const { adm1, adm2, adm3, watershed_name } = wpProfile;
+    if (wp) {
+      const { adm1, adm2, adm3, watershed_name } = wp;
       setWsTable({ adm1, adm2, adm3, watershed_name });
     }
-  }, [wpProfile]);
+  }, [wp]);
 
   // Generate the pdf based on a component
   const downloadProfileAsPdf = async () => {
@@ -88,8 +81,27 @@ function Waterprofile() {
     });
     const img = canvas.toDataURL("image/png");
     report.addImage(img, "JPEG", 20, 20, html.offsetWidth, html.offsetHeight);
-    report.save(`Profile_${wpProfile.name}.pdf`);
+    report.save(`Profile_${wp.name}.pdf`);
   };
+
+  const renderWpItem = (item, type, index, title) => {
+    if (type === "icon-x") {
+      return <ItemIconX item={item} key={index} />;
+    } else if (type === "icon-y") {
+      return <ItemIconY item={item} key={index} />;
+    } else if (type === "text") {
+      return <ItemText item={item} key={index} />;
+    } else if (type === "table") {
+      return <ItemTable item={item} key={index} title={title} />;
+    } else if (type === "simple-list") {
+      return <ItemSimpleList item={item} key={index} />;
+    } else if (type === "complex-list") {
+      return <ItemComplexList item={item} key={index} />;
+    } else {
+      return <></>;
+    }
+  };
+
   return (
     <>
       {idWater ? (
@@ -113,24 +125,24 @@ function Waterprofile() {
                 <Carousel
                   fade
                   controls={false}
-                  interval={1000}
+                  interval={5000}
                   pause={false}
                   className="w-100"
                 >
                   <Carousel.Item>
                     <img src={bgImg} className="w-100 img-carousel" />
                     <Carousel.Caption>
-                      <h5 className="fw-medium">{`${wpProfile.adm1}, ${wpProfile.adm2}, ${wpProfile.adm3}, ${wpProfile.watershed_name}`}</h5>
-                      <h1 className="fw-normal my-2">{wpProfile.name}</h1>
+                      <h5 className="fw-medium">{`${wp.adm1}, ${wp.adm2}, ${wp.adm3}, ${wp.watershed_name}`}</h5>
+                      <h1 className="fw-normal my-2">{wp.name}</h1>
                       <p className="fw-normal">
-                        {t("profile.area")}: {wpProfile.area} ha <br />{" "}
+                        {t("profile.area")}: {wp.area} ha <br />{" "}
                         {t("profile.population")}:{" "}
                         {
-                          wpProfile.contents_wp
+                          wp.contents_wp
                             .find((e) => e.title === "general")
                             .values.find((e) => "population" in e)["population"]
                         }{" "}
-                        <br /> Lat: {wpProfile.lat}, Lon: {wpProfile.lon}
+                        <br /> Lat: {wp.lat}, Lon: {wp.lon}
                       </p>
                       <div className="d-flex justify-content-end ">
                         <Button
@@ -149,17 +161,17 @@ function Waterprofile() {
                       className="w-100 img-carousel"
                     />
                     <Carousel.Caption>
-                      <h5 className="fw-medium">{`${wpProfile.adm1}, ${wpProfile.adm2}, ${wpProfile.adm3}, ${wpProfile.watershed_name}`}</h5>
-                      <h1 className="fw-normal my-2">{wpProfile.name}</h1>
+                      <h5 className="fw-medium">{`${wp.adm1}, ${wp.adm2}, ${wp.adm3}, ${wp.watershed_name}`}</h5>
+                      <h1 className="fw-normal my-2">{wp.name}</h1>
                       <p className="fw-normal">
-                        {t("profile.area")}: {wpProfile.area} ha <br />{" "}
+                        {t("profile.area")}: {wp.area} ha <br />{" "}
                         {t("profile.population")}:{" "}
                         {
-                          wpProfile.contents_wp
+                          wp.contents_wp
                             .find((e) => e.title === "general")
                             .values.find((e) => "population" in e)["population"]
                         }{" "}
-                        <br /> Lat: {wpProfile.lat}, Lon: {wpProfile.lon}
+                        <br /> Lat: {wp.lat}, Lon: {wp.lon}
                       </p>
                       <div className="d-flex justify-content-end ">
                         <Button
@@ -176,9 +188,9 @@ function Waterprofile() {
               <Container className="mt-3">
                 <Row>
                   <Col className="col-12 col-md-8">
-                    <h5 className="fw-medium">{t("profile.map")}</h5>
+                    <h4 className="fw-medium">{t("profile.map")}</h4>
                     <MapContainer
-                      center={[wpProfile.lat, wpProfile.lon]}
+                      center={[wp.lat, wp.lon]}
                       zoom={8}
                       style={{
                         height: "400px",
@@ -203,47 +215,169 @@ function Waterprofile() {
                         </LayersControl.Overlay>
                       </LayersControl>
                     </MapContainer>
-                    <h5 className="mt-4 mb-3">{t("profile.watershed")}</h5>
-                    {wsLeft?.length > 0 &&
-                      wsLeft.map((e, index) => {
-                        return (
-                          <WaterpointItem type={e.type} item={e} key={index} />
-                        );
-                      })}
-                    <h5 className="mt-4 mb-3">{t("profile.waterpoint")}</h5>
-                    {wpLeft?.length > 0 &&
-                      wpLeft.map((e, index) => {
-                        return (
-                          <WaterpointItem type={e.type} item={e} key={index} />
-                        );
-                      })}
+                    <h4 className="mt-4 mb-3">{t("profile.watershed")}</h4>
+
+                    {wpProfile?.contents_ws.length > 0 && (
+                      <div>
+                        {wpProfile.contents_ws
+                          .reduce((acc, e, index) => {
+                            if (e.position === "left") {
+                              const existingGroup = acc.find(
+                                (group) =>
+                                  group.typecontent_name === e.typecontent_name
+                              );
+
+                              if (existingGroup) {
+                                existingGroup.items.push(e);
+                              } else {
+                                acc.push({
+                                  typecontent_name: e.typecontent_name,
+                                  items: [e],
+                                });
+                              }
+                            }
+                            return acc;
+                          }, [])
+                          .map((group, groupIndex) => (
+                            <div key={groupIndex}>
+                              <h5 className="text-capitalize mt-3">
+                                {group.typecontent_name}
+                              </h5>
+                              {group.items.map((item, itemIndex) => (
+                                <div key={itemIndex}>
+                                  {/* Renderiza tus elementos aquí */}
+                                  {renderWpItem(item, item.type, itemIndex)}
+                                </div>
+                              ))}
+                            </div>
+                          ))}
+                      </div>
+                    )}
+                    <h4 className="mt-4 mb-3">{t("profile.waterpoint")}</h4>
+
+                    {wpProfile?.contents_wp.length > 0 && (
+                      <div>
+                        {wpProfile.contents_wp
+                          .reduce((acc, e, index) => {
+                            if (e.position === "left") {
+                              const existingGroup = acc.find(
+                                (group) =>
+                                  group.typecontent_name === e.typecontent_name
+                              );
+
+                              if (existingGroup) {
+                                existingGroup.items.push(e);
+                              } else {
+                                acc.push({
+                                  typecontent_name: e.typecontent_name,
+                                  items: [e],
+                                });
+                              }
+                            }
+                            return acc;
+                          }, [])
+                          .map((group, groupIndex) => (
+                            <div key={groupIndex}>
+                              <h5 className="text-capitalize mt-3">
+                                {group.typecontent_name}
+                              </h5>
+                              {group.items.map((item, itemIndex) => (
+                                <div key={itemIndex}>
+                                  {/* Renderiza tus elementos aquí */}
+                                  {renderWpItem(item, item.type, itemIndex)}
+                                </div>
+                              ))}
+                            </div>
+                          ))}
+                      </div>
+                    )}
                   </Col>
                   <Col className="col-12 col-md-4">
-                    <h5 className="text-capitalize ">
+                    <h4 className="text-capitalize ">
                       {t("profile.watershed-d")}
-                    </h5>
-                    {wsTable && (
-                      <WaterpointItem
-                        item={wsTable}
-                        type="table"
-                        title="Watershed description"
-                      />
+                    </h4>
+                    {wsTable &&
+                      renderWpItem(
+                        wsTable,
+                        "table",
+                        0,
+                        "Watershed description"
+                      )}
+                    {wpProfile?.contents_ws.length > 0 && (
+                      <div>
+                        {wpProfile.contents_ws
+                          .reduce((acc, e, index) => {
+                            if (e.position === "right") {
+                              const existingGroup = acc.find(
+                                (group) =>
+                                  group.typecontent_name === e.typecontent_name
+                              );
+
+                              if (existingGroup) {
+                                existingGroup.items.push(e);
+                              } else {
+                                acc.push({
+                                  typecontent_name: e.typecontent_name,
+                                  items: [e],
+                                });
+                              }
+                            }
+                            return acc;
+                          }, [])
+                          .map((group, groupIndex) => (
+                            <div key={groupIndex}>
+                              <h5 className="text-capitalize mt-3">
+                                {group.typecontent_name}
+                              </h5>
+                              {group.items.map((item, itemIndex) => (
+                                <div key={itemIndex}>
+                                  {/* Renderiza tus elementos aquí */}
+                                  {renderWpItem(item, item.type, itemIndex)}
+                                </div>
+                              ))}
+                            </div>
+                          ))}
+                      </div>
                     )}
-                    {wsRight?.length > 0 &&
-                      wsRight.map((e, index) => {
-                        return (
-                          <WaterpointItem type={e.type} item={e} key={index} />
-                        );
-                      })}
-                    <h5 className="text-capitalize ">
+                    <h4 className="text-capitalize ">
                       {t("profile.waterpoint-d")}
-                    </h5>
-                    {wpRight?.length > 0 &&
-                      wpRight.map((e, index) => {
-                        return (
-                          <WaterpointItem type={e.type} item={e} key={index} />
-                        );
-                      })}
+                    </h4>
+                    {wpProfile?.contents_wp.length > 0 && (
+                      <div>
+                        {wpProfile.contents_wp
+                          .reduce((acc, e, index) => {
+                            if (e.position === "right") {
+                              const existingGroup = acc.find(
+                                (group) =>
+                                  group.typecontent_name === e.typecontent_name
+                              );
+
+                              if (existingGroup) {
+                                existingGroup.items.push(e);
+                              } else {
+                                acc.push({
+                                  typecontent_name: e.typecontent_name,
+                                  items: [e],
+                                });
+                              }
+                            }
+                            return acc;
+                          }, [])
+                          .map((group, groupIndex) => (
+                            <div key={groupIndex}>
+                              <h5 className="text-capitalize mt-3">
+                                {group.typecontent_name}
+                              </h5>
+                              {group.items.map((item, itemIndex) => (
+                                <div key={itemIndex}>
+                                  {/* Renderiza tus elementos aquí */}
+                                  {renderWpItem(item, item.type, itemIndex)}
+                                </div>
+                              ))}
+                            </div>
+                          ))}
+                      </div>
+                    )}
                   </Col>
                 </Row>
               </Container>
@@ -261,7 +395,7 @@ function Waterprofile() {
                 type="button"
                 className="btn btn-primary me-5 rounded-4"
                 to="/historicaldata"
-                state={{ idWater: wpProfile.id }}
+                state={{ idWater: wp.id }}
               >
                 <img src={dataIcon} alt="" className="me-3" />
                 {t("monitoring.data")}

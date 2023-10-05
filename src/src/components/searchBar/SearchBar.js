2ddab410b90807ade4d6429e140026b3d187911c
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-10-05 15:06:47 -0500

    EWP-136 search bar implemented with adm1, adm2 and adm3

--- src/src/components/searchBar/SearchBar.js
+++ src/src/components/searchBar/SearchBar.js
@@ -14,19 +14,21 @@ function SearchBar({ waterpoints, onWpClick }) {
   };
 
   const filteredWaterpoints = waterpoints.filter((wp) =>
-    wp.name.toLowerCase().includes(filterText.toLowerCase())
+    [wp.name, wp.adm1, wp.adm2, wp.adm3].some((prop) =>
+      prop.toLowerCase().includes(filterText.toLowerCase())
+    )
   );
 
   const handleWpClick = (wp) => {
     onWpClick(wp);
-    setSelectedWaterpoint(wp.name);
+    setSelectedWaterpoint(`${wp.name}, ${wp.adm3}, ${wp.adm2}, ${wp.adm1}`);
   };
 
   return (
     <div className="bar-hints">
       <div
         className={`search-bar d-flex px-3 py-2 bg-white align-items-center border-bottom ${
-          filteredWaterpoints.length === waterpoints.length
+          filterText === "" || filterText.length < 3
             ? "search-bar-unfocused "
             : "search-bar-focused "
         }`}
@@ -34,6 +36,7 @@ function SearchBar({ waterpoints, onWpClick }) {
         <input
           type="search"
           className="form-control form-control-sm border-0 text-input"
+          style={{ width: "350px" }}
           aria-label="Search waterpoint"
           placeholder={t("monitoring.placeholder-search")}
           onChange={handleFilterChange}
@@ -42,12 +45,12 @@ function SearchBar({ waterpoints, onWpClick }) {
         />
         <IconSearch />
       </div>
-      {filterText !== "" && (
-        <div className="bg-white waterpoints-search px-3 py-2">
+      {filterText !== "" && filterText.length >= 3 && (
+        <div className="bg-white waterpoints-search px-3 py-1">
           {filteredWaterpoints.map((wp, i) => {
             return (
               <div
-                className="mb-2 small"
+                className="py-1 small hint-div text-capitalize "
                 key={i}
                 onClick={() => handleWpClick(wp)}
               >
@@ -55,7 +58,7 @@ function SearchBar({ waterpoints, onWpClick }) {
                   className="me-3"
                   style={{ color: "#6ecdeb" }}
                 />
-                {wp.name}
+                {`${wp.name.toLowerCase()}, ${wp.adm3.toLowerCase()}, ${wp.adm2.toLowerCase()}, ${wp.adm1.toLowerCase()}`}
               </div>
             );
           })}

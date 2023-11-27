ee9223973c3a13ab1de55b174ffc7fb0fa6d7cea
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-11-27 09:37:01 -0500

    EWP-141 order of partner logo changed

--- src/src/components/footer/Footer.js
+++ src/src/components/footer/Footer.js
@@ -5,11 +5,11 @@ import partner2 from "../../assets/img/partner2.png";
 import partner3 from "../../assets/img/partner3.png";
 import partner4 from "../../assets/img/partner4.png";
 import partner5 from "../../assets/img/partner5.png";
-import "./Footer.css"
-import { useTranslation } from 'react-i18next';
+import "./Footer.css";
+import { useTranslation } from "react-i18next";
 
 function Footer() {
-  const [t, i18n] = useTranslation("global")
+  const [t, i18n] = useTranslation("global");
   return (
     <footer>
       <Container>
@@ -26,11 +26,31 @@ function Footer() {
 
           <Col className="d-flex flex-column flex-md-row align-items-center justify-content-between mt-3 d-lg-block col-lg-auto mt-lg-0">
             <p>{t("footer.partners")}:</p>
-            <img src={partner2} alt="partner Alliance" className="mx-3 my-2 my-md-0" />
-            <img src={partner4} alt="partner Minister" className="mx-3 my-2 my-md-0" />
-            <img src={partner1} alt="partner EIAR" className="me-3 mb-2 mb-md-0" />
-            <img src={partner3} alt="partner Bill & Melinda Gates" className="mx-3 my-2 my-md-0" />     
-            <img src={partner5} alt="partner CGIAR" className="mx-3 my-2 my-md-0" />
+            <img
+              src={partner1}
+              alt="partner EIAR"
+              className="me-3 mb-2 mb-md-0"
+            />
+            <img
+              src={partner5}
+              alt="partner CGIAR"
+              className="mx-3 my-2 my-md-0"
+            />
+            <img
+              src={partner2}
+              alt="partner Alliance"
+              className="mx-3 my-2 my-md-0"
+            />
+            <img
+              src={partner4}
+              alt="partner Minister"
+              className="mx-3 my-2 my-md-0"
+            />
+            <img
+              src={partner3}
+              alt="partner Bill & Melinda Gates"
+              className="mx-3 my-2 my-md-0"
+            />
           </Col>
         </Row>
       </Container>

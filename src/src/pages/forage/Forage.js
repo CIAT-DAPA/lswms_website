8d2cd85316f5650b6bdfe83aca84b8d09dcf81a1
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-12-20 15:54:41 -0500

    timeline controller added

--- src/src/pages/forage/Forage.js
+++ src/src/pages/forage/Forage.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./Forage.css";
 import {
   LayersControl,
@@ -6,44 +6,25 @@ import {
   TileLayer,
   WMSTileLayer,
   ZoomControl,
+  useMap,
 } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-timedimension";
 import Configuration from "../../conf/Configuration";
 
 function Forage() {
-  const mapRef = useRef(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      const map = mapRef.current;
-      const wmsLayer = L.tileLayer.wms(
-        "https://geo.aclimate.org/geoserver/agroclimate_indices_ao/wms",
-        {
-          layers: "agroclimate_indices_ao:coffee_number_dry_days",
-          format: "image/png",
-          transparent: true,
-        }
-      );
-      // Add WMS layer to the layer group
-      wmsLayer.addTo(map);
-    }, 500); // 500ms delay
-  }, []);
-
   return (
     <>
       <MapContainer
         center={[9.149175, 40.498867]}
-        zoom={4}
+        zoom={6}
         style={{
           height: "100vh",
           width: "100%",
         }}
         className="map-monitoring"
         zoomControl={false}
-        ref={mapRef}
       >
-        <ZoomControl position="topright" />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -59,9 +40,58 @@ function Forage() {
             />
           </LayersControl.Overlay>
         </LayersControl>
+        <ZoomControl position="topright" />
+        <AddWMSLayer />
       </MapContainer>
     </>
   );
+}
+
+function AddWMSLayer() {
+  const map = useMap();
+  const timeDimensionControlRef = useRef(null);
+
+  useEffect(() => {
+    const wmsLayer = L.tileLayer.wms(Configuration.get_url_geoserver(), {
+      layers: "waterpoints_et:biomass",
+      format: "image/png",
+      transparent: true,
+    });
+
+    // Create a time dimension
+    const timeDimension = new L.TimeDimension({
+      time: "2022-01", // Start the timeline at this date
+      timeInterval: "2022-01/2023-09", // Change this to match your data
+      period: "P1M", // Change this to match your data
+    });
+    map.timeDimension = timeDimension;
+
+    // Create and add a TimeDimension Layer to the map
+    var tdWmsLayer = L.timeDimension.layer.wms(wmsLayer, {
+      updateTimeDimension: true,
+      updateTimeDimensionMode: "replace",
+      timeDimensionName: "time", // Change this to the name of the time dimension in the WMS server
+      requestTimeFromCapabilities: true, // Add this line
+    });
+    tdWmsLayer.addTo(map);
+
+    // Create and add a TimeDimension Control to the map
+    if (!timeDimensionControlRef.current) {
+      var timeDimensionControl = new L.Control.TimeDimension({
+        timeDimension: timeDimension,
+        position: "bottomleft",
+        autoPlay: false,
+        playerOptions: {
+          buffer: 1,
+          minBufferReady: -1,
+        },
+      });
+      map.addControl(timeDimensionControl);
+      timeDimensionControlRef.current = timeDimensionControl;
+    }
+  }, [map]);
+
+  return null;
 }
 
 export default Forage;
